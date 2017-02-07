c01969303ff53d623e49dac6dc95f4cb95a902cb
nguyenvandatb <nguyen.van.datb@framgia.com>
2017-02-07 15:57:47 +0700

    amend bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params    # Not the final implementation!
+    @user.activated = true
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      log_in @user
+      #@user.send_activation_email
+      #flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render "new"
